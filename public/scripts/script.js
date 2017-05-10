92cfe494062b0d23d1259b09d4895488ec12b7a8
Sean Felling <Snfelling7@gmail.com>
2017-05-10 15:16:17 -0500

    all bugs were fixed and formatted

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,29 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
-var vm = this;
-vm.addRecord = function(){
-var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-vm.nameIn ='';
-vm.locationIn='';
-};
-vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
+var myApp = angular.module('myApp', []);
+myApp.controller('WhereMyPeeps', ['$http', function($http) {
+    var vm = this;
+    vm.addRecord = function() {
+        var objectToSend = {
+            name: vm.nameIn,
+            location: vm.locationIn
+        };
+        $http({
+            method: 'POST',
+            url: '/testPost',
+            data: objectToSend
+        });
+        vm.nameIn = '';
+        vm.locationIn = '';
+    };
+    vm.getRecords = function() {
+        $http({
+                method: 'GET',
+                url: '/getRecords',
+            }).then(function(response) {
+                vm.allTheRecords = response;
+                console.log(vm.allTheRecords);
+            }),
+            function myError(err) {
+                console.log(err.statusText);
+            }
+    };
 }]);
